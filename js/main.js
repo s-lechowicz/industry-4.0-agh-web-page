60849a35e61ccd4e6d32f017948175e8e63a7c1c
Sebastian Lechowicz <s-lechowicz@outlook.com>
2018-04-26 01:58:34 +0200

    Initialize IO

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var statusText = document.getElementById("status").innerText;
 
-if (statusText === "Available") {
+if (statusText === "0") {
     document.getElementById("submit-button").disabled = false;
     document.getElementById("submit-button").style.cursor = "pointer";
     document.getElementById("status").style.color = "#0F0";
@@ -10,8 +10,8 @@ if (statusText === "Available") {
     document.getElementById("status").style.color = "#F00";
 }
 
-$(document).ready(function() {
-    $(window).on("scroll", function() {
+$(document).ready(function () {
+    $(window).on("scroll", function () {
         if ($(window).scrollTop() >= 100) {
             $(".navbar-brand").addClass("shrink");
             $(".navbar").addClass("shrink").addClass("bg-dark");
@@ -21,3 +21,41 @@ $(document).ready(function() {
         }
     });
 });
+
+var pageByTagIdOutputMap = new Map();
+var pageByTagIdPostMap = new Map();
+
+// Output Map
+pageByTagIdOutputMap.set("status", "IO/IO_isBusy.html");
+
+// Form Post Map
+pageByTagIdPostMap.set("selectedItem", "IO/IO_item.html");
+
+$(document).ready(function () {
+    $.ajaxSetup({cache: false});
+    setInterval(function () {
+        for (var [tag, page] of pageByTagIdOutputMap) {
+            getIO(tag, page)
+        }
+    }, 1000);
+});
+
+function getIO(id, page) {
+    $.get(page, function (result) {
+        $('#' + id).text(result.trim());
+    });
+}
+
+$("#submit-button").click(function () {
+    for (var [tag, page] of pageByTagIdPostMap) {
+        postIO(tag, page)
+    }
+});
+
+function postIO(id, page) {
+    var name = '"webdata".' + page.substring(4, page.length - 5);
+    val = $('input[id=' + id + ']').val();
+    sdata = escape(name) + '=' + val;
+    $.post(page, sdata, function (result) {
+    });
+}
