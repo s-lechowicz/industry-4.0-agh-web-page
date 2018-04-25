3f03346547697604a2dc4ac5f4875311b9dc0b83
Sebastian Lechowicz <s-lechowicz@outlook.com>
2018-04-26 01:45:26 +0200

    Design header.

--- js/main.js
+++ js/main.js
@@ -9,3 +9,15 @@ if (statusText === "Available") {
     document.getElementById("submit-button").disabled = true;
     document.getElementById("status").style.color = "#F00";
 }
+
+$(document).ready(function() {
+    $(window).on("scroll", function() {
+        if ($(window).scrollTop() >= 100) {
+            $(".navbar-brand").addClass("shrink");
+            $(".navbar").addClass("shrink").addClass("bg-dark");
+        } else {
+            $(".navbar-brand").removeClass("shrink");
+            $(".navbar").removeClass("bg-dark");
+        }
+    });
+});
