38a030516dd74f31764cbe208589b959735aa7fd
Sebastian Lechowicz <s-lechowicz@outlook.com>
2018-05-15 02:50:53 +0200

    Third Version of application.
    * Speed up.
    * Reduce number of requests.
    * Prepare test output file.

--- js/main.js
+++ js/main.js
@@ -1,52 +1,55 @@
-function updateButton() {
-    var statusText = document.getElementById("status").innerText;
+var tagToYpdateArray = ["status",
+    "customerItem",
+    "customerName",
+    "customerSurname",
+    "st10_msg",
+    "st20_msg",
+    "st30_msg",
+    "st40_msg",
+    "st50_msg",
+    "st60_msg",
+    "st70_msg",
+    "st80_msg"];
 
+function updateButton(statusText) {
     if (statusText == "0") {
         document.getElementById("submit-button").disabled = false;
         document.getElementById("submit-button").style.cursor = "pointer";
         document.getElementById("status").style.color = "#0F0";
+        document.getElementById("status").innerText = "Available";
     } else {
         document.getElementById("submit-button").style.cursor = "not-allowed";
         document.getElementById("submit-button").disabled = true;
         document.getElementById("status").style.color = "#F00";
+        document.getElementById("status").innerText = "Running...";
     }
 }
 
 $(document).ready(function () {
-    updateButton();
+    updateButton("1");
+
     $(window).on("scroll", function () {
-        shrinkNavbar($(window).scrollTop() >= 100);
-        makeNavbarDark($(window).scrollTop() >= 100);
-        shrinkNavbarVertical($(this).width() < 640 && $(window).scrollTop() < 100);
+        updateNavbar();
     });
 
     $(window).on('resize', function () {
-        shrinkNavbarVertical($(this).width() < 640 && $(window).scrollTop() < 100);
+        updateNavbar();
     });
 
-    shrinkNavbar($(window).scrollTop() >= 100);
-    makeNavbarDark($(window).scrollTop() >= 100);
-    shrinkNavbarVertical($(this).width() < 640 && $(window).scrollTop() < 100);
+    updateNavbar();
 });
 
 $(document).ready(function () {
     $.ajaxSetup({cache: false});
     setInterval(function () {
-        for (var [tag, page] of pageByTagIdOutputMap) {
-            getIO(tag, page);
-        }
-        updateButton();
+        updateWebpage();
     }, 1000);
 });
 
-function getIO(id, page) {
-    $.get(page, function (result) {
-        var textFromPage = htmlDecode(result.trim());
-        if (textFromPage[0] == "'" && textFromPage.length > 3) {
-            textFromPage = textFromPage.substring(1, textFromPage.length - 1);
-        }
-        $('#' + id).text(textFromPage);
-    });
+function updateNavbar() {
+    shrinkNavbar($(window).scrollTop() >= 100);
+    makeNavbarDark($(window).scrollTop() >= 100);
+    shrinkNavbarVertical($(this).width() < 640 && $(window).scrollTop() < 100);
 }
 
 function htmlDecode(input) {
@@ -77,4 +80,25 @@ function makeNavbarDark(bool) {
     } else {
         $(".navbar").removeClass("bg-dark");
     }
+}
+
+function updateWebpage() {
+    // $.get("output-test.html", function (result) {
+    $.get("output.html", function (result) {
+        var output = JSON.parse(result);
+
+        for (var tag of tagToYpdateArray) {
+            var outputValue = htmlDecode(output[tag]).trim();
+
+            if (tag == "status") {
+                updateButton(outputValue);
+                continue;
+            }
+
+            if (outputValue[0] == "'" && outputValue.length > 3) {
+                outputValue = outputValue.substring(1, outputValue.length - 1);
+            }
+            $('#' + tag).text(outputValue);
+        }
+    });
 }
